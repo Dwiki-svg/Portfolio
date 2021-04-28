aa5e8ad73ccde07cd2d6106ef081ec3a8b212c2a
Dwiki-svg <62027968+Dwiki-svg@users.noreply.github.com>
2021-04-28 21:31:30 +0700

    portfolio

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+import { Fade } from "react-reveal";
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+
+export default function Footer() {
+  return (
+    <div className="footer-div">
+      <Fade>
+      <p className="footer-text">No Hp <span role="img">❤️</span><b>082171873561</b></p>
+        <p className="footer-text">Hello <span role="img">❤️</span> by Dwiky Firmansyah</p>
+      </Fade>
+    </div>
+  );
+}
